89aaa55bde41ecf60d1232b8f086f099be33f0e7
片淵 雄介 <katabutiyusuke@spurz.local>
2014-10-07 10:58:20 +0900

    Initial Commit

---
+++ SpriteTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpriteTest
+//
+//  Created by 片淵 雄介 on 2014/10/07.
+//  Copyright (c) 2014年 Spurs_katabuti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ SpriteTestTests/SpriteTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  SpriteTestTests.swift
+//  SpriteTestTests
+//
+//  Created by 片淵 雄介 on 2014/10/07.
+//  Copyright (c) 2014年 Spurs_katabuti. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SpriteTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
